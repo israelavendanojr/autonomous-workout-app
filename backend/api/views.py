4d12b80e6554051bdc966083614b06af97de210c
israelavendanojr <iajr0207@gmail.com>
2025-06-25 18:32:59 -0700

    Create note views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
 
-# Create your views here.
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            # add author because it is read only in serializer
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+class NoteDelete(generics.DestroyAPIView):
+    # no need to define queryset bc we are using get_queryset
+    serializer_class = NoteSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+
 class CreateUserView(generics.CreateAPIView):
     # Here list of all diff objs when creating new one
     queryset = User.objects.all()
@@ -13,3 +39,5 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     # Who can actually call this
     permission_classes = (AllowAny,)
+
+        
