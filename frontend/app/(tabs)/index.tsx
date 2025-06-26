1fd5469da28173395beb03f1935678448da7a642
israelavendanojr <iajr0207@gmail.com>
2025-06-26 10:26:15 -0700

    Nativewind working

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import "@/global.css"
 
 export default function HomeScreen() {
   return (
