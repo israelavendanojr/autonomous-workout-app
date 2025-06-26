b7a89564fa5b9c40b89e4975d8682b8c9a8c62c9
israelavendanojr <iajr0207@gmail.com>
2025-06-25 17:58:51 -0700

    JWT

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    # Here list of all diff objs when creating new one
+    queryset = User.objects.all()
+    # Tells view what data we need to accept
+    serializer_class = User
+    # Who can actually call this
+    permission_classes = (AllowAny,)
