2e94a6a0989b51aa7bc1e620decb112b85dc0163
israelavendanojr <iajr0207@gmail.com>
2025-06-26 18:17:27 -0700

    Ignoring JWT for now, need momentum

--- frontend/app/(tabs)/tab2.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Tab2Screen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-green-600">Tab 2</Text>
-    </View>
-  );
-}
