ba96bf28f34b24c0fdc8ed82d9a66a9fa78dbdd7
israelavendanojr <iajr0207@gmail.com>
2025-06-28 15:02:17 -0700

    Scenario agent running

--- backend/chatbot/engine/coodinator.py
+++
@@ -1,8 +0,0 @@
-from agents.legal_aid_agent import LegalAidAgent
-
-def main():
-    legal_aid_agent = LegalAidAgent()
-    legal_aid_agent.run()
-
-if __name__ == "__main__":
-    main()
--- backend/chatbot/engine/test_tools.py
+++
@@ -1,8 +0,0 @@
-from language_tool import translate_input, translate_output
-
-def main():
-    trans_prompt, native_lang = translate_input("¿Qué pasa si me detiene ICE?")
-    output = translate_output("I am a chatbot", native_lang)
-
-if __name__ == "__main__":
-    main()
