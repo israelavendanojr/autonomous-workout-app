393e22a33f31b61ec59c14e8de613ca15ab980e9
israelavendanojr <iajr0207@gmail.com>
2025-07-03 17:52:09 -0700

    Remove chatbot app, project redirect

--- backend/chatbot/engine/coordinator.py
+++
@@ -1,9 +0,0 @@
-from chatbot.engine.agents.scenario_agent import ScenarioAgent
-
-def main():
-    scenario_agent = ScenarioAgent()
-    scenario_context = scenario_agent.run()
-    print(scenario_context)
-
-if __name__ == "__main__":
-    main()
