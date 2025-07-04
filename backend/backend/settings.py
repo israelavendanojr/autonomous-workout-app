7053ae56cb444b8c59074e8f57484c6a5ecb87ff
israelavendanojr <iajr0207@gmail.com>
2025-07-03 18:14:45 -0700

    added user + plans apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,10 +55,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "api",
     "rest_framework",
     "corsheaders",
-    "chatbot",
+    "users",
+    "plans",
 ]
 
 MIDDLEWARE = [
