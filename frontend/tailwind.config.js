8345b8bbac30a58874c18a2d962c80aa1c77b7d8
israelavendanojr <iajr0207@gmail.com>
2025-07-05 16:21:49 -0700

    tailwind swap

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require("nativewind/preset")],
   content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./screens/**/*.{js,ts,jsx,tsx}",
+    "./global.css",
   ],
   theme: {
     extend: {},
